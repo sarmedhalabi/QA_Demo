d0f2bd08420d922e5b3c639b19f7b8c35c8b4e5e
sarmed <sarmedhalabi@gmail.com>
2021-01-17 23:47:44 -0500

    adding new lines

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -3,10 +3,18 @@ describe(' my first test case', function(){
     it('my first test case',function(){
 
 
+        cy.visit("https://www.amazon.com/")
+        cy.get('#twotabsearchtextbox').type('cars')
+
+        cy.get('#nav-search-submit-button').click()
+
+
+
+
     }
 
 
-//Hello
+
 
 
     )
